1be491ab479ca2250fa63d038f50dbf259f9999e
Shaurya Rajput <145199330+shaurya2425@users.noreply.github.com>
2025-08-01 10:34:42 +0530

    Add files via upload

--- Lab Assignment 2/q1.py
+++ Lab Assignment 2/q1.py
@@ -1,1 +1,17 @@
+# Initialize the list
+friends = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Helen", "Ivy", "Jack"]
 
+#a. Print the first name.
+print("First name: ",friends[0])
+
+#b. Print the last name.
+print("Last name: ",friends[-1])
+
+# c. Print names from third to fifth index (i.e., index 3 to 5)
+print("Names from index 3 to 5:", friends[3:6])
+
+# d. Print names in reverse order
+print("Names in reverse order:", friends[::-1])
+
+#e. Print names from 8th to 1st in reverse order.
+print("Names from 8th to 1st in reverse order: ", friends[8:0:-1])
